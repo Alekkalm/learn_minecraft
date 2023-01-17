1b31330609605ccaa7f4fec1495500873449d442
AlekseyK <alekkalm@gmail.com>
2023-01-17 13:58:31 +0300

    добавил коментарий

--- MinecraftConsts.h
+++ MinecraftConsts.h
@@ -7,7 +7,7 @@
 
 namespace MinecraftConsts {
     const double WORLD_SCALE = 10; //масштабирование кубиков и скорости
-    const double REACH_RANGE = 10; //максимальная длинна с которой мы можем ставить и ломать блоки
+    const double REACH_RANGE = 10; //максимальная длинна с которой мы можем ставить и ломать блоки (измеряется в кубиках)
 
     const double GRAVITY = 20;
     const double JUMP_HEIGHT = 1.2;
